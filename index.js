841af33a81700abf67e04ca8f5054e26dca721c3
Diego Souza <diegoazevedo0807@gmail.com>
2024-11-25 14:58:36 -0300

    Mudanças das novas rotas e htmls

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.get("/", function(req,res){
     res.send("Bem vindo ao meu site");
 })
 
 app.get("/produtos", function(req,res){
-    res.send("<h1>Lista de produtos<h1>");
+    const filePath = path.join(__dirname, "produtos.html");
+    res.sendFile(filePath);
 })
 
 app.get("/sobre", function(req,res){
-    res.send("<h1>Loja fundada em 2023<h1>")
+    const filePath = path.join(__dirname, "sobre.html");
+    res.sendFile(filePath);
 })
 
 app.get("/produtos/camisas", function(req,res){
-    res.send("<h1>Camisas<h1>")
+    const filePath = path.join(__dirname, "camisas.html");
+    res.sendFile(filePath);
+})
+
+app.get("/produtos/calcas", function(req,res){
+    const filePath = path.join(__dirname, "calcas.html");
+    res.sendFile(filePath);
 })
 
 app.get("/consulta/:parametro", function(req,res){
