f5efb411aa51a97dc4a44020dc7da85944fd1749
Diego Souza <diegoazevedo0807@gmail.com>
2024-12-04 18:17:52 -0300

    Atualizando codigo

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
 const express = require("express");
-const app = express();
+const { createClient } = require("@supabase/supabase-js");
 const path = require("path");
+const app = express();
 
-app.get("/", function(req,res){
-    res.send("Bem vindo ao meu site");
-})
+const SUPABASE_URL = "https://wmtvgjgfowixbddomhaz.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndtdHZnamdmb3dpeGJkZG9taGF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMzNDA2NjQsImV4cCI6MjA0ODkxNjY2NH0.lPoTQBR00zGzfVW-UT7esrlk293WzxnN-ucJOndBCIg";
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-app.get("/produtos", function(req,res){
-    const filePath = path.join(__dirname, "produtos.html");
-    res.sendFile(filePath);
-})
+app.use(express.json()); // Middleware para processar JSON
 
-console.log("Hello World");
+/** app.post("/produtos", async function (req, res) {
+    const { nome, preco } = req.body;
+    const { data, error } = await supabase
+        .from("produtos")
+        .insert([{ nome, preco }]);
 
-app.get("/sobre", function(req,res){
-    const filePath = path.join(__dirname, "sobre.html");
-    res.sendFile(filePath);
-})
-
-app.get("/produtos/camisas", function(req,res){
-    const filePath = path.join(__dirname, "camisas.html");
-    res.sendFile(filePath);
-})
-
-app.get("/produtos/calcas", function(req,res){
-    const filePath = path.join(__dirname, "calcas.html");
-    res.sendFile(filePath);
-})
-
-app.get("/consulta/:parametro", function(req,res){
-    res.send("retorno consulta:" + req.params.parametro);
-})
-
-app.listen(4000,function(erro){
-    if (erro){
-        console.log("Erro ao iniciar")
-    } else {
-        console.log("Servidor inciado com sucesso");
+    if (error) {
+        return res.status(400).send({ error: error.message });
     }
-})
+    res.status(201).send(data);
+}); **/
+
+app.get("/produtos", async function (req, res) {
+    const { data, error } = await supabase
+        .from("produtos")
+        .select("*");
+
+    if (error) {
+        return res.status(400).send({ error: error.message });
+    }
+    res.status(200).send(data);
+});
+
+app.get("/produtos/:name", async function (req, res) {
+    const { name } = req.params; // Obter o ID da URL
+    const { data, error } = await supabase
+        .from("produtos") // Nome da tabela no Supabase
+        .select("*")
+        .eq("name", name) // Filtrar pelo campo `id`
+
+    if (error) {
+        return res.status(400).send({ error: error.message });
+    }
+
+    if (data.length === 0) {
+        return res.status(404).send({ error: "Produto não encontrado" });
+    }
+
+    res.status(200).send(data[0]); // Enviar o primeiro resultado como resposta
+});
+
+app.listen(4000, function (erro) {
+    if (erro) {
+        console.log("Erro ao iniciar");
+    } else {
+        console.log("Servidor iniciado com sucesso");
+    }
+});
